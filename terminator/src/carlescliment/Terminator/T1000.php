d6a411c0f52800302043cf109b45f1396a03d30d
Carles Climent <carlescliment@hotmail.com>
2013-09-29 23:21:29 +0200

    it moves to target through calculated route

--- terminator/src/carlescliment/Terminator/T1000.php
+++ terminator/src/carlescliment/Terminator/T1000.php
@@ -4,20 +4,24 @@ namespace Terminator;
 
 use Terminator\Routing\Calculator;
 use Terminator\Entities\Target;
+use Terminator\Components\Legs;
 
 class T1000
 {
     private $routeCalculator;
 
 
-    public function __construct(Calculator $route_calculator)
+    public function __construct(Calculator $route_calculator, Legs $legs)
     {
         $this->routeCalculator = $route_calculator;
+        $this->legs = $legs;
     }
 
 
     public function addTarget(Target $target)
     {
-        $this->routeCalculator->calculate($target);
+        $route = $this->routeCalculator->calculate($target);
+        $this->legs->move($route);
     }
+
 }
--- terminator/tests/Terminator/Test/T1000Test.php
+++ terminator/tests/Terminator/Test/T1000Test.php
@@ -14,12 +14,35 @@ class T1000Test extends \PHPUnit_Framework_TestCase
     public function itCalculatesTheStraightRouteToTarget()
     {
         $route_calculator = $this->getMock('Terminator\Routing\Calculator');
-        $t1000 = new T1000($route_calculator);
+        $legs = $this->getMock('Terminator\Components\Legs');
+        $t1000 = new T1000($route_calculator, $legs);
         $target = $this->getMock('Terminator\Entities\Target');
 
         $route_calculator->expects($this->once())
             ->method('calculate')
             ->with($target);
+
+        $t1000->addTarget($target);
+    }
+
+
+    /**
+     * @test
+     */
+    public function itMovesToTheTargetThroughTheCalculatedRoute()
+    {
+        $route_calculator = $this->getMock('Terminator\Routing\Calculator');
+        $legs = $this->getMock('Terminator\Components\Legs');
+        $t1000 = new T1000($route_calculator, $legs);
+        $target = $this->getMock('Terminator\Entities\Target');
+        $route = $this->getMock('Terminator\Routing\Route');
+        $route_calculator->expects($this->any())
+            ->method('calculate')
+            ->will($this->returnValue($route));
+
+        $legs->expects($this->once())
+            ->method('move')
+            ->with($route);
 
         $t1000->addTarget($target);
     }
