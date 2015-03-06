23c59be857641f7937fa88c3ba34c08976954d71
Carles Climent <carlescliment@hotmail.com>
2015-03-06 17:12:17 +0100

    Extract class CSVSource

--- katas/reader/loader.rb
+++ katas/reader/loader.rb
@@ -3,10 +3,20 @@ require 'ostruct'
 require 'csv'
 
 module Loader
+  class CSVSource
+    def initialize(path)
+      @path = path
+    end
+
+    def rows
+      CSV.read(@path)
+    end
+  end
+
   def self.load(path)
     entries = []
     layout = Layout.new
-    rows = CSV.read(path)
+    rows = CSVSource.new(path).rows
     rows.each do |row|
       entries << OpenStruct.new(layout.extract(row)) if layout.valid?(row)
     end
