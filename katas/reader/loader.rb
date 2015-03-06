64076dd1e89101653ec1595def50a4722d4c781b
Carles Climent <carlescliment@hotmail.com>
2015-03-06 16:19:51 +0100

    Extract Balance field

--- katas/reader/loader.rb
+++ katas/reader/loader.rb
@@ -1,15 +1,25 @@
 require 'ostruct'
 require 'csv'
 
+
 module Loader
+  module Fields
+    class Balance
+      def valid?(value)
+        value =~ /[0-9]+\.[0-9]{2}/
+      end
+    end
+  end
+
   def self.load(path)
     entries = []
     CSV.foreach(path) do |row|
+      balance = Fields::Balance.new
       entries << OpenStruct.new({
         name: row[0].strip,
         balance: row[1].to_f,
         account_number: row[2]
-      }) if row[1] =~ /[0-9]+\.[0-9]{2}/
+      }) if balance.valid?(row[1])
     end
 
     entries
